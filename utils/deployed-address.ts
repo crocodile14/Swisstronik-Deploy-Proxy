a8fb62157b856ce86a6def160d46936ca9458727
crocodile14 <149391751+crocodile14@users.noreply.github.com>
2024-08-06 07:04:56 +0700

    Add files via upload

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x5DB75780D9B25D72bE01243202D9ba8cED4c59a5'
+export const ProxyAdmin = '0xFAba163Dc0fa9BF4278A719a0bd3cDabAdB53C24'
+export const SWTRImplementation = '0xA7BA21d5eE59668F0aa72960053D95a4Bda8e55B'
+    
